63a5abdc91fd6dd57d4c37b2ef5b1571558c8473
13AM <79609111+Covarun@users.noreply.github.com>
2021-11-10 08:18:37 -0800

    Update xp.ts

--- src/commands/xp.ts
+++ src/commands/xp.ts
@@ -1,24 +1,26 @@
 import { Command, CommandContext, Permission } from './command';
-import CommandUtils from '../utils/command-utils';
 
-export default class XPCommand implements Command {
-    name = 'xp';
-    summary = 'Display the XP card of a user.';
-    precondition: Permission = '';
-    cooldown = 3;
-    module = 'Leveling';
+import { getMemberFromMention } from '../utils/command-utils';
 
-    execute = (ctx: CommandContext, userMention: string) =>  {
-        const target = (userMention) ?
-            CommandUtils.getMemberFromMention(userMention, ctx.guild) : ctx.member;
-        
-        if (target.user.bot)
-            throw new Error(`Bot users cannot earn XP`);
+export default class implements Command {
+  aliases = ['level', 'profile'];
+  name = 'xp';
+  summary = 'Display the XP card of a user.';
+  precondition: Permission = '';
+  cooldown = 3;
+  module = 'Leveling';
 
-        const xpCardURL = `${process.env.API_URL}/bots/${ctx.bot.user.id}/guilds/${ctx.guild.id}/members/${target.id}/xp-card`;             
-        return ctx.channel.send({ files: [{
-            attachment: xpCardURL,
-            name: 'xp-card.png'
-        }]});
-    };
+  execute = (ctx: CommandContext, userMention: string) =>  {
+    const target = (userMention)
+      ? getMemberFromMention(userMention, ctx.guild)
+      : ctx.member;
+    
+    if (target.user.bot)
+      throw new TypeError(`Bot users cannot earn XP`);      
+
+    const xpCardURL = `${process.env.API_URL}/guilds/${ctx.guild.id}/members/${target.id}/xp-card`;
+    return ctx.channel.send(
+      { files: [{ attachment: xpCardURL, name: 'xp-card.png' }]
+    });
+  };
 }
